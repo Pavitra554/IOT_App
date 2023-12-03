04596ce0640c069d98c401f2b4913ec96ce4f241
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-12-03 11:43:00 +0530

    CREATE: App setup done

---
+++ android/app/src/main/kotlin/com/example/iot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:iot/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
