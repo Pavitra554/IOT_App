9b14789c2a502bc5231b0dfc62a95027a3e27559
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-12-03 18:18:47 +0530

    ADD: home screen added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,8 +10,37 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(),
-    );
+        backgroundColor: Colors.black,
+        body: Container(
+            alignment: Alignment.center,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Home Screen',
+                  style: TextStyle(
+                      color: Colors.white24,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Under Development ',
+                      style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Icon(
+                      Icons.warning_amber_outlined,
+                      size: 25,
+                      color: Colors.orangeAccent,
+                    ),
+                  ],
+                ),
+              ],
+            )));
   }
 }
